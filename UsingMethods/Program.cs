477fdd6b1e35e8073b733bd60beeb1b0f8da490d
Matthew Lamb <lamb.matthew@gmail.com>
2015-09-17 08:54:38 -0500

    Added UsingMethods examples.

---
+++ UsingMethods/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingMethods
+{
+    class Program
+    {
+        private static int userNumber1, userNumber2;
+        private static string message = "";
+        static void Main(string[] args)
+        {            
+            
+            GetTwoNumbers();
+            message = ChangeMessage();
+            Console.WriteLine("This message was generated using the ChangeMessage() method that returns a string: {0}", message);
+            ChangeMessage(message);
+            Console.WriteLine("This message was generated using the ChangeMessage() method that returns nothing: {0}", message);
+            Console.ReadLine();
+
+            
+        }
+        /// <summary>
+        /// Checks to see which userNumber is bigger and returns a string saying greater than,
+        /// less than or equal to
+        /// </summary>
+        /// <returns>string message</returns>
+        private static string ChangeMessage()
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks to see which userNumber is bigger and accepts a paramater of type string to modify to
+        /// greater than, less than or equal to
+        /// </summary>
+        /// <param name="message">string to modify</param>
+        private static void ChangeMessage(String message)
+        {
+
+        }
+
+        /// <summary>
+        /// Example method. This code could just have easily been in main but here we use a method to illustrate
+        /// the concept of code reuse. 
+        /// </summary>
+        private static void GetTwoNumbers()
+        {
+            Console.Write("Enter a number: ");
+            userNumber1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n\r");
+            Console.Write("Enter a second number: ");
+            userNumber2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n\r");
+        }
+    }
+}
--- VariablesPractice/Program.cs
+++ VariablesPractice/Program.cs
@@ -10,12 +10,16 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            //TODO: Change these variable declarations to be more specific.
+            //      Change the variable names as well to be more descriptive.
             var variable1 = 10;
             var variable2 = .10F;
             var variable3 = .0010D;
             var variable4 = 'A';
             var variable5 = "Variable";
 
+
+            //TODO: Change these console statements to reflect the new variable names you entered above.
             Console.WriteLine("This variable is: {0}", variable1.GetType());
             Console.WriteLine("This variable is: {0}", variable2.GetType());
             Console.WriteLine("This variable is: {0}", variable3.GetType());
