9f99be0579398bf7f642a887cc6ec277f05aa188
Matthew Lamb <lamb.matthew@gmail.com>
2015-09-16 20:47:18 -0500

    Added VariablePractice

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var variable1 = 10;
+            var variable2 = .10F;
+            var variable3 = .0010D;
+            var variable4 = 'A';
+            var variable5 = "Variable";
+
+            Console.WriteLine("This variable is: {0}", variable1.GetType());
+            Console.WriteLine("This variable is: {0}", variable2.GetType());
+            Console.WriteLine("This variable is: {0}", variable3.GetType());
+            Console.WriteLine("This variable is: {0}", variable4.GetType());
+            Console.WriteLine("This variable is: {0}", variable5.GetType());
+            Console.ReadLine();
+        }
+    }
+}
