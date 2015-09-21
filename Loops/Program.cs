d47f4c4525f7768a52d594f0f0cfbda129fd6231
Matthew Lamb <lamb.matthew@gmail.com>
2015-09-20 23:39:01 -0500

    Added basic structure to Loops, but still deciding on premise to be taught.

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -10,6 +10,22 @@ namespace Loops
     {
         static void Main(string[] args)
         {
+            int num1 = 10;
+            int num2 = 11;
+            for (int i = 0; i < num1; i++)
+            {
+                //How many times will this loop run?
+            }
+
+            for (int i = num1; i < num2; i++)
+            {
+                // How many times will this loop run?
+            }
+
+            for (int i = 1; i < num2; i++)
+            {
+                // How many times will this loop run?
+            }
         }
     }
 }
